473f2da91e0ef27b01869b2b85371390543a13e0
Catalina Picco <catalina.picco@gmail.com>
2017-05-27 11:42:20 -0300

    subi archivos al repositorio

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//siempre se hace un document ready para que la pagina cargue el js antes de funcionar.
+$( document ).ready(function() {
+  console.log( "Jquery Cargado" );
+});
+
+console.log("app.js fue cargado con exito!");
